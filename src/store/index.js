fd994231bd52d1e96505d872211b90f54c182aa1
Gulli <gulli301185@gmail.com>
2023-06-25 11:40:44 +0600

    provided

---
+++ src/store/index.js
@@ -0,0 +1,25 @@
+import { configureStore } from "@reduxjs/toolkit"
+import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE } from "redux-persist"
+import persistReducer from "redux-persist/es/persistReducer"
+import persistStore from "redux-persist/es/persistStore"
+import storage from 'redux-persist/lib/storage'
+import rootReducer from './RootReducer'
+
+
+const persistConfig={
+    key:'Trello',
+    storage:storage,
+}
+
+const persistedReducer=persistReducer(persistConfig, rootReducer)
+
+export const store=configureStore({
+    reducer:persistedReducer,
+    middleware:(getDefaultMiddleware)=>getDefaultMiddleware({
+        serializableCheck:{
+            ignoreActions:[FLUSH,REHYDRATE,PAUSE,PERSIST,PURGE,REGISTER]
+        }
+    })
+})
+
+export const persistor= persistStore(store)
