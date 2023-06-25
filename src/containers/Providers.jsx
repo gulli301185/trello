8b642ab096b2d43e596c69c8060b67b14d723ec1
Gulli <gulli301185@gmail.com>
2023-06-25 12:19:59 +0600

    fixed

--- src/containers/Providers.jsx
+++ src/containers/Providers.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "../store";
 import { Provider } from "react-redux";
-import Theme from "../Theme";
+import Theme from "../theme";
 import { CssBaseline } from "@mui/material";
 
 const Providers = ({ children }) => {
